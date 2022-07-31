929ff0fcf8a3abad3f62be31daa9965486645b91
ThinamXx <thinamjr1@gmail.com>
2022-07-31 18:44:53 +0545

    Completed the app.py

--- MLOps Zoomcamp/06. Model Monitoring/README.md
+++ MLOps Zoomcamp/06. Model Monitoring/README.md
@@ -13,3 +13,19 @@
 
 ### **Monitoring Pipeline**
 `ML Service + Usage Simulation + Logging + Online Monitoring + Batch Monitoring + Container`
+
+
+### **Monitoring Pipeline Instructions**
+
+- Create a virtual environment and install the `requirements.txt` file.  
+```python3
+pipenv --python=3.10.4
+pipenv shell
+pipenv install -r requirements.txt
+```  
+  
+- Run the commands:  
+```python3
+python3 prepare.py 
+docker-compose up
+```
--- MLOps Zoomcamp/06. Model Monitoring/evidently_service/app.py
+++ MLOps Zoomcamp/06. Model Monitoring/evidently_service/app.py
@@ -4,8 +4,11 @@ DEMO SERVICE FOR EVIDENTLY METRICS INTEGRATION WITH PROMETHEUS AND GRAFANA:
 
 # IMPORTING MODULES:
 from ast import Str
+from asyncio.log import logger
 from curses import window
 import os
+import re
+from weakref import ref
 import yaml
 import hashlib
 import dataclasses
@@ -123,3 +126,94 @@ class MonitoringService:
         if current_size < window_size:
             logging.info(f"Not enough data for measurement: {current_size} of {window_size}" f" Waiting more data!")
             return
+        
+        next_run_time = self.next_run_time.get(dataset_name)
+
+        if next_run_time is not None and next_run_time > datetime.datetime.now():
+            logging.info(f"Next run for dataset {dataset_name} at {next_run_time}")
+            return
+        
+        self.next_run_time[dataset_name] = datetime.datetime.now() + datetime.timedelta(
+            seconds=self.calculation_period_sec
+        )
+
+        self.monitoring[dataset_name].execute(
+            self.reference[dataset_name], current_data, self.column_mapping[dataset_name]
+        )
+
+        for metric, value, labels in self.monitoring[dataset_name].metrics():
+            metric_key = f"evidently:{metric.name}"
+            found = self.metric.get(metric_key)
+
+            if not labels:
+                labels = {}
+            
+            labels["dataset_name"] = dataset_name
+
+            if isinstance(value, str):
+                continue
+
+            if found is None:
+                found = prometheus_client.Gauge(metric_key, "", list(sorted(labels.keys())))
+                self.metrics[metric_key] = found
+            
+            try:
+                found.labels(**labels).set(value)
+            
+            except ValueError as error:
+                logging.error("Value error for metric %s, error: ", metric_key, error)              # Ignore errors sending other metrics.
+
+
+SERVICE: Optional[MonitoringService] = None
+
+
+@app.before_first_request
+def configure_service():
+    # pylint: disable=global-statement
+    global SERVICE
+    config_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.yaml")
+
+    if not os.path.exists(config_file_path):
+        logging.error("File %s does not exist", config_file_path)
+        exit("Cannot find a config file for the metrics service. Follow README.md for setup instructions.")
+    
+
+    with open(config_file_path, "rb") as config_file:
+        config = yaml.safe_load(config_file)
+    
+    options = MonitoringServiceOptions(**config["service"])
+    datasets = {}
+
+    for dataset_name, dataset_options in config["datasets"].items():
+        reference_file = dataset_options["reference_file"]
+        logging.info(f"Load reference data for dataset {dataset_name} from {reference_file}.")
+        reference_data = pq.read_table(reference_file).to_pandas()
+        reference_data["duration"] = reference_data.lpep_dropoff_datetime  - reference_data.lpep_pickup_datetime
+        reference_data.duration = reference_data.duration.apply(lambda x: x.total_seconds() / 60)
+        reference_data = reference_data[(reference_data.duration >= 1) & (reference_data.duration <= 60)]
+        datasets[dataset_name] = LoadedDataset(
+            name=dataset_name,
+            references=reference_data,
+            monitors=dataset_options["monitors"],
+            column_mapping=ColumnMapping(**dataset_options["column_mapping"])
+        )
+        logging.info("Reference is loaded for dataset %s: %s rows", dataset_name, len(reference_data))
+    
+    SERVICE = MonitoringService(datasets=datasets, window_size=options.window_size)
+
+
+
+@app.route('/iterate/<dataset>', methods=['POST'])
+def iterate(dataset: str):
+    item = flask.request.json
+
+    global SERVICE
+    if SERVICE is None:
+        return "Internal Server Error: service not found", 500
+
+    SERVICE.iterate(dataset_name=dataset, new_rows=pd.DataFrame.from_dict(item))
+    return "Okay"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
