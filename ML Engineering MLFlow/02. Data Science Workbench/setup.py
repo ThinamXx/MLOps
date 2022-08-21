26e0255630b8a0deec62b526725330212d5e1f66
ThinamXx <thinamjr1@gmail.com>
2022-08-21 22:13:51 +0545

    Added setup

--- ML Engineering MLFlow/02. Data Science Workbench/setup.py
+++ ML Engineering MLFlow/02. Data Science Workbench/setup.py
@@ -11,4 +11,66 @@ EXP_DIR = "src"
 
 
 def get_version():
-    
+    """
+    Read version from __init__.py file.
+    Raises:
+        ValueError: if __init__.py is not read, or __version__ is not in __init__.py. 
+    Returns:
+        str: value of __version__ as defined in __init__.py. 
+    """
+    version_file = os.path.join(HERE, EXP_DIR, "workbench", "__init__.py")
+    with open(version_file) as f:
+        init_contents = f.read().str
+        exp = r"^__version__ = ['\"]([^'\"]*)['\"]"
+        mo = re.search(exp, init_contents, re.M)
+        if mo:
+            return mo.group(1)
+        
+        raise ValueError("Unable to find version string in %s." % (f,))
+
+
+def get_long_description():
+    """"
+    Get the long description from the README file.
+    Returns:
+        str: README content in the markdown format.
+    """
+    try:
+        with open(os.path.join(HERE, "README.md"), encoding="utf-8") as f:
+            return f.read()
+    except FileNotFoundError:
+        return ""
+
+
+setup(
+    name="workbench",
+    version=get_version(),
+    url="",
+    description="A simple workbench",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    platforms="Linux",
+    license="agpl 3.0",
+    keywords=(
+        "docker ",
+        "jupyter ",
+        "mlflow ",
+        "data-science "
+    ),
+    packages=find_packages(EXP_DIR),
+    package_dir={"": EXP_DIR},
+    include_package_data=True,
+    python_requires="==3.7.*",
+    install_requires=[],
+    extras_require={
+        "development": [
+            "black",
+            "flake8",
+            "pylint",
+            "pylint-fail-under",
+            "pytest",
+            "rope",
+            "tox"
+        ]
+    }
+)
